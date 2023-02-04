00cd6ae0ed71272e218cacb5a8009e19390ce498
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2023-02-04 14:10:06 +0330

    Ui Started.

--- WindowsDefenderControler/Form1.Designer.cs
+++ WindowsDefenderControler/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(687, 331);
+            this.ClientSize = new System.Drawing.Size(671, 331);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
