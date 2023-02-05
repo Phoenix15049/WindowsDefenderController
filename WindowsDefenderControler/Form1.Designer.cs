dd69092c1a7651a8ae61bced08093d4cf2b64d27
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2023-02-05 23:33:50 +0330

    Some Bugs fixed .

--- WindowsDefenderControler/Form1.Designer.cs
+++ WindowsDefenderControler/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsDefenderControler
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         /// Required designer variable.
@@ -30,13 +30,13 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(671, 331);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Name = "Form1";
+            this.Name = "Main";
             this.Text = "Windows Defender Controller";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
