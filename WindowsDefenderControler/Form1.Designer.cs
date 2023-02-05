9fea79e14762922a600bdc3cc728ca86218a4a67
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2023-02-05 23:27:58 +0330

    Titles added.

--- WindowsDefenderControler/Form1.Designer.cs
+++ WindowsDefenderControler/Form1.Designer.cs
@@ -37,7 +37,7 @@
             this.ClientSize = new System.Drawing.Size(671, 331);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Windows Defender Controller";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
