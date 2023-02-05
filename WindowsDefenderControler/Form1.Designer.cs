535c074a453aea712b94386ed557d16562d5e671
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2023-02-05 23:27:06 +0330

    Main functions added.

--- WindowsDefenderControler/Form1.Designer.cs
+++ WindowsDefenderControler/Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
