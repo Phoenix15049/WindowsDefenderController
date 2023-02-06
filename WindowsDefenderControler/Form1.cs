445e2917fd2651745c6615d57a2c5668e942d1cb
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2023-02-07 01:01:23 +0330

    Some bugs fixed.

--- WindowsDefenderControler/Form1.cs
+++ WindowsDefenderControler/Form1.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,7 +18,45 @@ namespace WindowsDefenderControler
         {
             InitializeComponent();
         }
+        void CRegistryDemoDlg::OnBnClickedSet()
+        {
+            // TODO: Add your control notification handler code here
+            CRegKey reg;
 
+            HKEY m_hKeyParent = HKEY_CURRENT_USER;
+
+            //Here I Was wrong while giving path
+            LPCTSTR m_myKey = L"Software\\MyRegDemo";
+
+            reg.Create(m_hKeyParent, m_myKey);
+
+            if (reg.Open(m_hKeyParent, m_myKey) == ERROR_SUCCESS)
+            {
+                DWORD data = 1;
+                reg.SetDWORDValue(L"Test", data);
+                reg.Close();
+            }
+        }
+
+        void CRegistryDemoDlg::OnBnClickedGet()
+        {
+            // TODO: Add your control notification handler code here
+            CRegKey reg;
+            HKEY m_hkeyParent = HKEY_CURRENT_USER;
+
+            //Here I Was wrong while giving path
+            LPCTSTR m_myKey = L"Software\\MyRegDemo";
+            DWORD dvalue;
+            if (reg.Open(m_hkeyParent, m_myKey) == ERROR_SUCCESS)
+            {
+                reg.QueryDWORDValue(L"Test", dvalue);
+                reg.Close();
+            }
+            if (dvalue == 1)
+                m_chkbox1.SetCheck(true);
+            else
+                m_chkbox1.SetCheck(false);
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
