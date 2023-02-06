525c7d4e458bb024961ec1bb7586cb83c43fcc6d
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2023-02-07 00:50:33 +0330

    labels added to UI.

--- WindowsDefenderControler/Form1.Designer.cs
+++ WindowsDefenderControler/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.Start = new System.Windows.Forms.Button();
             this.Help = new System.Windows.Forms.Button();
+            this.Srvce = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Start
@@ -52,12 +53,24 @@
             this.Help.UseVisualStyleBackColor = true;
             this.Help.Click += new System.EventHandler(this.Help_Click);
             // 
+            // Srvce
+            // 
+            this.Srvce.AutoSize = true;
+            this.Srvce.Font = new System.Drawing.Font("Nexa Rust Sans Black", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Srvce.ForeColor = System.Drawing.Color.Firebrick;
+            this.Srvce.Location = new System.Drawing.Point(134, 129);
+            this.Srvce.Name = "Srvce";
+            this.Srvce.Size = new System.Drawing.Size(328, 32);
+            this.Srvce.TabIndex = 2;
+            this.Srvce.Text = "Service is running";
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSkyBlue;
             this.ClientSize = new System.Drawing.Size(582, 253);
+            this.Controls.Add(this.Srvce);
             this.Controls.Add(this.Help);
             this.Controls.Add(this.Start);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -65,6 +78,7 @@
             this.Text = "Windows Defender Controller";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -72,6 +86,7 @@
 
         private System.Windows.Forms.Button Start;
         private System.Windows.Forms.Button Help;
+        private System.Windows.Forms.Label Srvce;
     }
 }
 
