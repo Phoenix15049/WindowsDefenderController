d5d61263ff42c4d4aa6257117fd4016d0f39986d
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2023-02-06 17:02:11 +0330

    Some bugs fixed.

--- WindowsDefenderControler/Form1.Designer.cs
+++ WindowsDefenderControler/Form1.Designer.cs
@@ -28,19 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.Start = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // Start
             // 
-            this.button1.Location = new System.Drawing.Point(158, 21);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(300, 74);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Start.Location = new System.Drawing.Point(158, 21);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(300, 74);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start / Stop";
+            this.Start.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
@@ -58,7 +57,7 @@
             this.BackColor = System.Drawing.Color.LightSkyBlue;
             this.ClientSize = new System.Drawing.Size(582, 253);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Start);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Main";
             this.Text = "Windows Defender Controller";
@@ -69,7 +68,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Start;
         private System.Windows.Forms.Button button2;
     }
 }
--- WindowsDefenderControler/Form1.cs
+++ WindowsDefenderControler/Form1.cs
@@ -17,14 +17,5 @@ namespace WindowsDefenderControler
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
