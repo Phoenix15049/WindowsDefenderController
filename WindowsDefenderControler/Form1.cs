e5944f4c92f46c21857207c9bf1a111fbc9f06a8
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2023-02-09 00:33:23 +0330

    subkey creator added.

--- WindowsDefenderControler/Form1.cs
+++ WindowsDefenderControler/Form1.cs
@@ -20,7 +20,43 @@ namespace WindowsDefenderControler
         {
             InitializeComponent();
         }
-       
+        
+        static RegistryKey createAllSubkey(string completeSubkey, RegistryKey rootreg)
+        {
+            RegistryKey _rtrn = null;
+            StringBuilder sb = new StringBuilder();
+            var stepreg = rootreg;
+            var stestr = "";
+            foreach (var item in completeSubkey.Split('\\'))
+            {
+                if (item != "")
+                {
+                    try
+                    {
+
+                        stestr += item + @"\";
+                        var reg = stepreg.OpenSubKey(item, true);
+                        if (reg == null)
+                        {
+                            stepreg.CreateSubKey(item);
+                            stepreg = stepreg.OpenSubKey(item, true);
+                        }
+                        else
+                        {
+                            stepreg = stepreg.OpenSubKey(item, true);
+                        }
+                    }
+                    catch (Exception exc)
+                    {
+                        Console.WriteLine(exc.Message);
+                    }
+                }
+                _rtrn = stepreg;
+            }
+
+            return _rtrn;
+        }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
