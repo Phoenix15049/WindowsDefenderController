66fb4fb85f93bd03b1a954de7480a81e85495c97
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2023-02-10 10:37:56 +0330

    Some bugs fixed.

--- WindowsDefenderControler/Form1.cs
+++ WindowsDefenderControler/Form1.cs
@@ -116,31 +116,7 @@ namespace WindowsDefenderControler
                 }
             }
 
-            using (RegistryKey
-                testSettings = test9999.OpenSubKey("TestSettings", true))
-            {
-                // Delete the ID value.
-                testSettings.DeleteValue("id");
-
-                // Verify the deletion.
-                Console.WriteLine((string)testSettings.GetValue(
-                    "id", "ID not found."));
-            }
-
-            // Delete or close the new subkey.
-            Console.Write("\nDelete newly created registry key? (Y/N) ");
-            if (Char.ToUpper(Convert.ToChar(Console.Read())) == 'Y')
-            {
-                Registry.CurrentUser.DeleteSubKeyTree("Test9999");
-                Console.WriteLine("\nRegistry key {0} deleted.",
-                    test9999.Name);
-            }
-            else
-            {
-                Console.WriteLine("\nRegistry key {0} closed.",
-                    test9999.ToString());
-                test9999.Close();
-            }
+            
 
         }
 
